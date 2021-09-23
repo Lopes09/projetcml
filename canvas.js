79ae84e9eef2f73baf2d7d39775a0c106bd40b8e
Labrice <dieunordlabrice111@gmail.com>
2021-09-23 15:55:26 -0400

    Fichier javascript

---
+++ canvas.js
@@ -0,0 +1,156 @@
+var contextcan=document.querySelector("#mycanvas").getContext("2d");
+
+		//finish
+		contextcan.beginPath();
+		contextcan.moveTo(605, 268);
+		contextcan.lineTo(605, 249);
+		contextcan.lineTo(630, 249);
+		contextcan.lineTo(630, 294);
+		contextcan.lineWidth=2;
+		contextcan.strokeStyle='#000';
+		contextcan.fillStyle='#ffb84f';
+		contextcan.fill();
+		contextcan.stroke();
+// debut
+		contextcan.beginPath();
+		contextcan.moveTo(480,370);
+		contextcan.lineTo(575,275);
+		contextcan.lineTo(667, 368);
+		contextcan.lineTo(680, 358);
+		contextcan.lineTo(575, 253);
+		contextcan.lineTo(470, 357);
+		contextcan.lineTo(480, 370);
+		contextcan.lineWidth=2;
+		contextcan.strokeStyle='#000';
+		contextcan.fillStyle='#1f1de2';
+		contextcan.fill();
+		contextcan.closePath();
+		contextcan.stroke();
+
+
+		contextcan.beginPath();
+		contextcan.moveTo(680, 358);
+		contextcan.lineTo(575, 256);
+		contextcan.lineTo(470, 358);
+		contextcan.lineTo(460, 348);
+		contextcan.lineTo(575, 241);
+		contextcan.lineTo(690, 346);
+		contextcan.lineTo(680, 358);
+		contextcan.lineWidth=2;
+		contextcan.strokeStyle='#000';
+		contextcan.fillStyle='#4a48fa';
+		contextcan.fill();
+		contextcan.closePath();
+		contextcan.stroke(); 
+
+			//finish
+		contextcan.beginPath();
+		contextcan.moveTo(500,350);
+		contextcan.lineTo(575,275);
+		contextcan.lineTo(650,350);
+		contextcan.lineTo(650,450);
+		contextcan.lineTo(500,450);
+		contextcan.lineTo(500,350);
+		contextcan.lineWidth=2;
+		contextcan.strokeStyle='#000';
+		contextcan.fillStyle='#f7ca37';
+		contextcan.fill();
+		contextcan.closePath();
+		contextcan.stroke();
+	
+		
+
+		
+
+		//finish
+
+		contextcan.beginPath();
+		contextcan.moveTo(600, 250);
+		contextcan.lineTo(635, 250);
+		contextcan.lineTo(635, 239);
+		contextcan.lineTo(600, 239);
+		contextcan.lineWidth=2;
+		contextcan.strokeStyle='#000';
+		contextcan.fillStyle='#4a48fa';
+		contextcan.fill();
+		contextcan.closePath();
+		contextcan.stroke();
+
+		//finsh
+
+		contextcan.beginPath();
+		contextcan.moveTo(530, 335);
+		contextcan.lineTo(575, 290);
+		contextcan.lineTo(619, 335);
+		contextcan.lineWidth=2;
+		contextcan.strokeStyle='#000';
+		contextcan.fillStyle='#bbeef4';
+		contextcan.fill();
+		contextcan.closePath();
+		contextcan.stroke();
+
+		//finish
+
+		contextcan.beginPath();
+		contextcan.moveTo(560, 305);
+		contextcan.lineTo(590, 305);
+		contextcan.lineTo(575, 291);
+		contextcan.lineTo(575, 335);
+		contextcan.lineWidth=2;
+		contextcan.strokeStyle='#000';
+		contextcan.fill();
+		contextcan.stroke();
+
+		//finsh
+
+		contextcan.beginPath();
+		contextcan.moveTo(510, 354);
+		contextcan.lineTo(510, 424);
+		contextcan.lineTo(535, 424);
+		contextcan.lineTo(535, 354);
+		contextcan.lineWidth=2;
+		contextcan.fillStyle='#bbeef4';
+		contextcan.fill();
+		contextcan.closePath();
+		contextcan.stroke();
+
+		//finsh
+		contextcan.beginPath();
+		contextcan.moveTo(545, 355);
+		contextcan.lineTo(545, 415);
+		contextcan.lineTo(605, 415);
+		contextcan.lineTo(605, 355);
+		contextcan.lineTo(575, 355);
+		contextcan.lineTo(575, 415);
+		contextcan.lineTo(575, 355);
+		contextcan.lineTo(544, 355);
+		contextcan.lineWidth=2;
+		contextcan.fillStyle='#bbeef4';//couleur fond
+		contextcan.fill();
+		contextcan.stroke();
+
+		//finish
+
+		contextcan.beginPath();
+		contextcan.moveTo(545, 413);
+		contextcan.lineTo(545, 423);
+		contextcan.lineTo(605, 423);
+		contextcan.lineTo(605, 413);
+		contextcan.lineWidth=2;//epaisseur ligne
+		contextcan.fillStyle='#ffb84f';//couleur fond
+		contextcan.fill();//fermeture couleur fond
+		contextcan.closePath();
+		contextcan.stroke();
+
+		//finish
+
+		contextcan.beginPath();
+		contextcan.moveTo(615, 355);
+		contextcan.lineTo(615, 425);
+		contextcan.lineTo(640, 425);
+		contextcan.lineTo(640, 355);
+		contextcan.lineWidth=2;//epaisseur ligne
+		contextcan.fillStyle='#bbeef4';//couleur fond
+		contextcan.fill();//fermeture couleur fond
+		contextcan.closePath();
+		contextcan.stroke();
